c25817b0426e42df8e5be6db7ef8f0149cb85a40
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2021-11-23 02:19:17 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street-42);
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return distanceFromHqInFeet(destination) - distanceFromHqInFeet(start);
+}
+
+function calculatesFarePrice(start, destination) {
+    let ftBillable = distanceTravelledInFeet(start, destination);
+    let fare = "";
+
+    if (ftBillable <= 400) {
+        fare = 0;
+    } else if (ftBillable < 2000) {
+        fare = (ftBillable - 400) * 0.02;
+    } else if (ftBillable < 2500) {
+        fare = 25;
+    } else if (ftBillable > 2500) {
+        fare = "cannot travel that far";
+    }
+    return fare;
+}
